7eec6696e4565453b338ee0796715424200a8218
Chen-Chih Lee <jacklee26@gmail.com>
2021-05-15 09:52:38 +0800

    updated

--- excport_Excel/2.exel_export_space_newRowColumn.py
+++ excport_Excel/2.exel_export_space_newRowColumn.py
@@ -1,10 +1,10 @@
 #-*-coding:utf-8 -*-
 
 import csv
-with open('file1.csv', 'wb') as csvfile:
+with open('file1.csv', 'w',  newline='', encoding="utf-8") as csvfile:
     spamwriter = csv.writer(csvfile, dialect='excel')
    
-    with open('vocab2.txt', 'rb') as filein:
+    with open('vocab2.txt', 'r', encoding="utf-8") as filein:
         for line in filein:
             line_list = line.strip('\n').split(' ')
             spamwriter.writerow(line_list)
